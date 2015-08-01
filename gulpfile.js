c5e5a4a113f225e13197eeeb9a90a8160f848c0f
Andrey Gordeev <jgordeev@gmail.com>
2015-08-01 16:59:27 +0700

    update README.md and packages for install

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
 var gulp = require('gulp'),
 	jade = require('gulp-jade'),
 	sass = require('gulp-sass'),
-	html2jade = require('gulp-html2jade'),
 	webserver = require('gulp-webserver'),
-	imagemin = require('gulp-imagemin'),
-	pngquant = require('imagemin-pngquant'),
-	iconfont = require('gulp-iconfont'),
 	notify = require('gulp-notify');
 
 
@@ -34,54 +30,53 @@ gulp.task('scss',  function() {
 		.pipe(gulp.dest('dist/css/'));
 });
 
- gulp.task('image', function() {
-     gulp.src('dist/img/*')
-       .pipe(gulp.dest('raw/images'))
-       .pipe(imagemin({
-       		progressive: true,
-       		use: [pngquant()]
-       }))
-       .pipe(gulp.dest('dist/img'));
- });
-
-
- gulp.task('convert', function(){
- 	gulp.src('raw/builder/html/*.html')
- 		.pipe(html2jade())
-		.pipe(notify('Шаблоны .jade обновлены'))
- 		.pipe(gulp.dest('raw/builder/jade'));
- });
-
-
 gulp.task('watch', function() {
    gulp.watch(['app/scss/*.scss', 'app/scss/*/*.scss', 'app/templates/*.jade', 'app/templates/*/*.jade'], ['scss', 'template']);
 });
 
 // other auxiliary tasks
- gulp.task('fonts', function(){
- 	gulp.src("app/icons/*.svg")
- 	.pipe(iconfont({
- 	  fontName: 'iconproject',
- 	  appendCodepoints: true  
- 	}))
- 	.on('codepoints', function(codepoints, options){
- 		console.log(codepoints, options);
- 	})
- 	.pipe(gulp.dest("dist/fonts/iconproject"))
- });
+gulp.task('image', function() {
+ gulp.src('dist/img/*')
+   .pipe(gulp.dest('raw/images'))
+   .pipe(imagemin({
+   		progressive: true,
+   		use: [pngquant()]
+   }))
+   .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('convert', function(){
+	gulp.src('raw/builder/html/*.html')
+		.pipe(html2jade())
+	.pipe(notify('Шаблоны .jade обновлены'))
+		.pipe(gulp.dest('raw/builder/jade'));
+});
+
+gulp.task('fonts', function(){
+	gulp.src("app/icons/*.svg")
+	.pipe(iconfont({
+	  fontName: 'iconproject',
+	  appendCodepoints: true  
+	}))
+	.on('codepoints', function(codepoints, options){
+		console.log(codepoints, options);
+	})
+	.pipe(gulp.dest("dist/fonts/iconproject"))
+});
 
 // include fonts
 gulp.task('tofonts1', function(){
- 	gulp.src("bower_components/fontawesome/fonts/*")
- 	.pipe(gulp.dest("dist/fonts/fontawesome"))
- });
+	gulp.src("bower_components/fontawesome/fonts/*")
+	.pipe(gulp.dest("dist/fonts/fontawesome"))
+});
 gulp.task('tofonts2', function(){
- 	gulp.src("bower_components/roboto-fontface/fonts/*")
- 	.pipe(gulp.dest("dist/fonts/roboto-fontface"))
- });
+	gulp.src("bower_components/roboto-fontface/fonts/*")
+	.pipe(gulp.dest("dist/fonts/roboto-fontface"))
+});
+// other tasks
+gulp.task('update', ['scss', 'template', 'image', 'fonts']);
+
 
 // default task
 gulp.task('default', ['webserver', 'scss', 'template', 'watch']);
 
-// other tasks
-gulp.task('update', ['scss', 'template', 'image', 'fonts']);
